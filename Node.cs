b1caef74e3a8ff41475e344634ad47e76d0b3f76
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-04-08 22:59:01 +0530

    [Manoj]Adding_ListInAscendingOrder

--- Node.cs
+++ Node.cs
@@ -4,6 +4,13 @@
     {
         public int data;//to store integer vaslue
         public Node next; //holding the address of the next node
+
+        public Node()
+        {
+        }
+
+
+
         //for initializing crerating a constructor
         public Node(int data)
         {
