f09106fff31f033a932479386326d60e8aeb9301
Nagasaimanoj8 <101272258+Nagasaimanoj8@users.noreply.github.com>
2022-04-08 23:07:34 +0530

    [Manoj]Adding_ListInAscendingOrder

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace Datastructure
             static void Main(string[] args)
             {
             //  LinkedList<int> my_line = new LinkedList<int>();
-            //my_line.AddLast(10);
+            //my_line.AddLast(20);
             //my_line.AddLast(70);
             //my_line.AddFirst(30);
             //CustomLinkedList linkedList = new CustomLinkedList();
