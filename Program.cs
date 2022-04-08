cab408e851de8ea7bd1cdd780b6c4659ec90752e
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-04-08 07:14:29 +0530

    [Manoj]Adding_Simple_Linked_List

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datastructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // LinkedList<int> my_line = new LinkedList<int>();
+           // my_line.AddLast(10);
+            // my_line.AddLast(70);
+             //my_line.AddFirst(300);
+            CustomLinkedList linkedList= new CustomLinkedList();
+            linkedList.AddAtLast(56);
+            linkedList.AddAtLast(30);
+            linkedList.AddAtLast(70);
+            linkedList.Display();
+              Console.ReadLine();                      
+        }
+           
+
+        }
+
+    }
